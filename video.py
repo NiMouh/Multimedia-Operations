4ec83800769d8d20e9cf624749c426af897f683c
NiMouh <simaoaugusto11@hotmail.com>
2022-01-10 10:02:34 +0000

    atualizações do video.py

--- video.py
+++ video.py
@@ -1,35 +1,90 @@
 import cv2
 
-# Inicializa o video
-video = cv2.VideoCapture('Video/video.mp4')
 
-# Determina a resolução do video capturado
-largura_quadro = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-altura_quadro = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-tamanho = (largura_quadro, altura_quadro)
+def pretoebrancovid(vid):
+    # Determina a resolução do video capturado
+    largura_quadro = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    altura_quadro = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    tamanho = (largura_quadro, altura_quadro)
 
-# Define o formato do video
-fourcc = cv2.VideoWriter_fourcc('M', 'P', '4', 'V')
+    # Define o formato do video
+    fourcc = cv2.VideoWriter_fourcc('M', 'P', '4', 'V')
+    fps = vid.get(cv2.CAP_PROP_FPS)
 
-# Inicializa o novo video
-novo_video = cv2.VideoWriter('Video/blacknwhite_video.mp4', fourcc, 30.0, tamanho)
+    # Inicializa o novo video
+    novo_video = cv2.VideoWriter('Video/blacknwhite_video.mp4', fourcc, fps, tamanho)
 
-while True:
+    while True:
 
-    # Leitura dos frames
-    existeQuadro, quadro = video.read()
+        # Leitura dos frames
+        existeQuadro, quadro = video.read()
 
-    if existeQuadro:
-        for x in range(0, largura_quadro - 1):
-            for y in range(0, altura_quadro - 1):
-                r, g, b = quadro[y, x]
-                cinza = r * 0.2125 + g * 0.7174 + b * 0.0721
-                quadro[y, x] = [cinza, cinza, cinza]
-        novo_video.write(quadro)
-    else:
-        break
+        if existeQuadro:
+            for x in range(0, largura_quadro - 1):
+                for y in range(0, altura_quadro - 1):
+                    r, g, b = quadro[y, x]
+                    cinza = r * 0.2125 + g * 0.7174 + b * 0.0721
+                    quadro[y, x] = [cinza, cinza, cinza]
+            novo_video.write(quadro)
+        else:
+            break
+    novo_video.release()
 
-# Encerra os videos e encerra todas as janelas
-video.release()
-novo_video.release()
-cv2.destroyAllWindows()
+
+def negativovid(vid):
+    # Determina a resolução do video capturado
+    largura_quadro = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    altura_quadro = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    tamanho = (largura_quadro, altura_quadro)
+
+    # Define o formato do video
+    fourcc = cv2.VideoWriter_fourcc('M', 'P', '4', 'V')
+    fps = vid.get(cv2.CAP_PROP_FPS)
+
+    # Inicializa o novo video
+    novo_video = cv2.VideoWriter('Video/negativo_video.mp4', fourcc, fps, tamanho)
+
+    while True:
+
+        # Leitura dos frames
+        existeQuadro, quadro = video.read()
+
+        if existeQuadro:
+            for x in range(0, largura_quadro - 1):
+                for y in range(0, altura_quadro - 1):
+                    r, g, b = quadro[y, x]
+                    quadro[y, x] = [255 - r, 255 - g, 255 - b]
+            novo_video.write(quadro)
+        else:
+            break
+    novo_video.release()
+
+
+def menu():
+    print("\n<--- Bem-vindo à edição de videos --->\n")
+    print("1: Transformar um video no seu preto e branco.")
+    print("2: Transformar um video no seu negativo.")
+    print("0: Terminar.\n")
+    escolha = int(input("Escolha uma opção: "))
+    return escolha
+
+
+if __name__ == "__main__":
+    opcao = menu()
+
+    while opcao != 0:
+
+        # Inicializa o video
+        video = cv2.VideoCapture('Video/video.mp4')
+
+        if opcao == 1:
+            pretoebrancovid(video)
+            print("Operação 1 foi concluída.")
+        elif opcao == 2:
+            negativovid(video)
+            print("Operação 2 foi concluída.")
+        else:
+            print("Opção inválida.")
+
+        # Volta a printar o menu
+        opcao = menu()
